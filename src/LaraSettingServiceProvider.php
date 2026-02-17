40915478db8da59e9a041f3c0067b516633162f8
amirz <amirhz.dev@gmail.com>
2026-02-18 01:21:53 +0330

    mod: change migration, models and all publishes tag

--- src/LaraSettingServiceProvider.php
+++ src/LaraSettingServiceProvider.php
@@ -14,16 +14,16 @@ class LaraSettingServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__ . '/Database/Migrations/create_settings_table.php.stub' => database_path('migrations/' . date('Y_m_d_His') . '_create_settings_table.php')
-        ], 'webafra-settings-migrations');
+        ], 'migrations');
 
         $this->publishes([
             __DIR__ . '/Models/' => app_path('Models/Webafra'),
-        ], 'webafra-settings-models');
+        ], 'models');
 
         $this->publishes([
             __DIR__ . '/Database/Migrations/' => database_path('migrations'),
             __DIR__ . '/Models/' => app_path('Models/Webafra'),
-        ], 'webafra-settings-all');
+        ], 'all');
     }
 
     /**
