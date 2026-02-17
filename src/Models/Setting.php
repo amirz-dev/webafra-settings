dcd65fb727dded99ebcfe0363dd18b1209b8eb31
amirz <amirhz.dev@gmail.com>
2026-02-18 02:12:10 +0330

    feat: Update Setting migration and model to support 'group' attribute with default constants and query scope

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -1,17 +1,32 @@
 <?php
+
 namespace Webafra\LaraSetting\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
+    // default constants
+    public const GROUP_SITE    = 'site';
+    public const GROUP_EMAIL   = 'email';
+    public const GROUP_PAYMENT = 'payment';
+    public const GROUP_CUSTOM  = 'custom';
+
     protected $fillable = [
         'key',
         'value',
         'is_primary',
+        'group'
     ];
 
     protected $casts = [
         'is_primary' => 'boolean',
     ];
+
+    
+    /* ================= Scopes ================= */
+    public function scopeGroup($query, string $group)
+    {
+        return $query->where('group', $group);
+    }
 }
